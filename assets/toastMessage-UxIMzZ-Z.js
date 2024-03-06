faf3062be12dcda349d2447e3ae7190a55893179
emillo <yc.gd1688@gmail.com>
2024-03-07 01:15:22 +0800

    Updates

---
+++ assets/toastMessage-UxIMzZ-Z.js
@@ -0,0 +1,1 @@
+import{q as o}from"./index-P73iaSj8.js";const h=o("toastMessage",{state:()=>({messages:[]}),actions:{toastShow(){setTimeout(()=>{this.messages.shift()},6e3)},clearToast(s){this.messages.splice(s,1)},pushMessage(s){const{style:e="success",title:t,content:a}=s;this.messages.push({style:e,title:t,content:a}),this.toastShow()}}});export{h as u};
