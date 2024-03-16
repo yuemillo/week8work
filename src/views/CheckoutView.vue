896229d840286e3e6fd1cb3f189c1030f30af661
emillo <yc.gd1688@gmail.com>
2024-03-17 01:07:08 +0800

    結帳圖片修改

--- src/views/CheckoutView.vue
+++ src/views/CheckoutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container d-flex flex-column align-items-center">
-    <img src="/public/_7a1ce4f6-c3f9-45d2-a3b7-94d83cd263da.jpg" alt="" style="width:800px;height: 600px;" class="object-fit-cover mt-3">
+    <img src="/7a1ce4f6-c3f9-45d2-a3b7-94d83cd263da.jpg" alt="" style="width:800px;height: 600px;" class="object-fit-cover mt-3">
     <h2 class="py-3">已獲得英雄喵喵</h2>
   </div>
 </template>
